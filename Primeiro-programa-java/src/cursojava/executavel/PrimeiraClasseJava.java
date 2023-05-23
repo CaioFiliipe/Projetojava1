c86e4fd3887ab95a76ce1bca1f44f3185f727c8e
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-22 21:05:22 -0300

    Criação do Projeto

---
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*main é um metodo auto ecxecutavel em java*/
+    public static void main(String[] args) {
+    	System.out.println("meu primeiro progama em java");
+	   System.out.println("uai cuidaaaa");
+    	System.out.println("chamaaa");
+    	System.out.println("caio");
+	   System.out.println(" quem diria eu aqui sozinho aprendendo a ser programador");
+}
+}
