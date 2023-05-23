8f420d5d67fc4ef3296d0970b8bdced16199983f
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-22 22:47:02 -0300

    fazendo operações entre numeros interios

--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,25 +1,22 @@
 package cursojava.executavel;
-/*aksak*/
+
 public class PrimeiraClasseJava {
 	
-	/*variavel global é acessivel a todos*/
-	static int maiorIdadeGlobal = 30;
 	
 	/*main é um metodo auto ecxecutavel em java*/
     public static void main(String[] args) {
-     
-   /* Variavel local porque pertence somente a esse metodo*/
-    int maiorIdade = 18;
-    System.out.println("valor da variavel local=" + maiorIdade);
-    System.out.println("valor da variavel local=" + maiorIdadeGlobal);
-    
-    metodo2();
+  
+   int nota1 = 90;
+   int nota2 = 70;
+   int nota3 = 50;
+   int nota4 = 88;
+  
+    System.out.println("Media das notas=" + (nota1 + nota2 + nota3 + nota4)/4 );
+   
     
     
    }
     
-    public static void metodo2 () {
-      System.out.println("valor da variavel global =" + maiorIdadeGlobal);
-    }
+   
 }
 
