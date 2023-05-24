bcf45a6422aa0344b46fb320c155482ce7e144cf
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-23 23:14:17 -0300

    continuação das operaçoes logicas com IF e ELSE

--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,17 +5,23 @@ public class PrimeiraClasseJava {
 	/* main é um metodo auto ecxecutavel em java */
 	public static void main(String[] args) {
 
-		int mediaAluno = 70;
-        String nome = "fabio";
-		
-		if (mediaAluno >= 70 && nome.equals("Caio")) {
-			System.out.println("Parabens vc está aprovado");
+		int nota1 = 10;
+		int nota2 = 60;
+		int nota3 = 10;
+		int nota4 = 10;
+		int media = 0;
 
-		} else if (mediaAluno < 70) {
-			System.out.println("vc esta reprovado");        
-		}else {
-			System.out.println("Aluno não encontrado");
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+		if (media >= 70) {
+			System.out.println("aluno Aprovado: " + media);
+		} 
+		if (media >= 40 && media <= 69) {
+			System.out.println("Aluno em Recuperaçao: " + media);
+		}
+		else {
+			System.out.println("Aluno Reprovado: " + media);
+		}
+
 	}
-
-}
 }
