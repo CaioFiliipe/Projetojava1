c51f5cec1eab5a059a4e3eff5fefb1de8d2ca7bf
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-23 22:21:55 -0300

    concatenação de variaves

--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,13 +6,15 @@ public class PrimeiraClasseJava {
 	/*main é um metodo auto ecxecutavel em java*/
     public static void main(String[] args) {
   
-   //String no java sempre será um texto de tamanho qualquer de caracteres	
-    String textoQualquer ="as asdjajsdjl asdjklajsldkj asljdlkajsdljalsj e por ai vai ";
+   //Concatenação = unir ou juntar dados
+   
     
-    String none;
-    String cpf;
-    String telefone;
-    String endereco;
+    String nome = "Caio Filipe";
+    String cpf = "000.000.000.00";
+    String telefone = "81 99999-9999";
+    String endereco = "Recife-PE";
+    int idade = 27;
+   System.out.println(" Meu Nome é: " + nome + " Do CPF:" + cpf + " Meu contato é: " + telefone + " Moro em: " +  endereco + " E tenho de idade: "+idade );
     
     
    }
