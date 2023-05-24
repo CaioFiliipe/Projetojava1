48513c4381b41c9f7b410280a6d0c624ac6e0171
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-23 23:01:16 -0300

    Operaçoes logicas com IF e ELSE

--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,24 +1,21 @@
 package cursojava.executavel;
 
 public class PrimeiraClasseJava {
-	
-	
-	/*main é um metodo auto ecxecutavel em java*/
-    public static void main(String[] args) {
-  
-   //Concatenação = unir ou juntar dados
-   
-    
-    String nome = "Caio Filipe";
-    String cpf = "000.000.000.00";
-    String telefone = "81 99999-9999";
-    String endereco = "Recife-PE";
-    int idade = 27;
-   System.out.println(" Meu Nome é: " + nome + " Do CPF:" + cpf + " Meu contato é: " + telefone + " Moro em: " +  endereco + " E tenho de idade: "+idade );
-    
-    
-   }
-    
-   
-}
 
+	/* main é um metodo auto ecxecutavel em java */
+	public static void main(String[] args) {
+
+		int mediaAluno = 70;
+        String nome = "fabio";
+		
+		if (mediaAluno >= 70 && nome.equals("Caio")) {
+			System.out.println("Parabens vc está aprovado");
+
+		} else if (mediaAluno < 70) {
+			System.out.println("vc esta reprovado");        
+		}else {
+			System.out.println("Aluno não encontrado");
+	}
+
+}
+}
