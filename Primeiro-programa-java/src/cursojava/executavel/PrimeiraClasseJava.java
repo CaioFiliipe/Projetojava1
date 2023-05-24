986248490e6874b81d8336c37e110887c6c0f37a
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-23 21:38:57 -0300

    tipo CHAR de caracteres

--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,16 +6,18 @@ public class PrimeiraClasseJava {
 	/*main é um metodo auto ecxecutavel em java*/
     public static void main(String[] args) {
   
-   double nota1 = 90;
-   double nota2 = 70;
-   double nota3 = 50;
-   double nota4 = 88;
-   
-   
-   double mediaFinal = (nota1 + nota2 + nota3 + nota4)/4;
-  
-    System.out.println("Media das notas=" + mediaFinal );
-   
+   /*CHAR representa uma unica letra ex  'F' uma letra dento de aspas simples*/
+    	
+    	char pessoaFeminino = 'F';
+    	
+    	if(pessoaFeminino == 'F') {
+    		System.out.println("Feminino");
+    	}else {
+    		System.out.println("Masculino");
+    	}
+    	
+    	
+    	
     
     
    }
