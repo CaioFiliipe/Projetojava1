5bd3c94b0934280a3bac6f0f1c044f0175978fb7
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-25 20:17:46 -0300

    Operadores relacionais ( == , != , <,> , <= , >= )

--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,41 +5,15 @@ public class PrimeiraClasseJava {
 	/* main é um metodo auto ecxecutavel em java */
 	public static void main(String[] args) {
 
-		int nota1 = 70;
-		int nota2 = 70;
-		int nota3 = 80;
-		int nota4 = 60;
-		int media = 0;
-
-		media = (nota1 + nota2 + nota3 + nota4) / 4;
-		// SWITCH CASE :OPERAÇOES EXATAS
-		int dia = 1;
-		
-		switch (dia) {
-		case 1:
-			System.out.println("domingo");
-			break;
-		case 2:
-			System.out.println("segunda-feira");
-			break;
-		case 3:
-			System.out.println("terça-feira");
-			break;
-		case 4:
-			System.out.println("quarta-feira");
-			break;
-		case 5:
-			System.out.println("quinta-feira");
-			break;
-		case 6:
-			System.out.println("sexta-feira");
-			break;
-		case 7:
-			System.out.println("sabado");
-			break;
-		default:System.out.println("Outro dia qualquer");
-			break;
-		}
+	int nota1 = 100;
+	int nota2 = 100;
+	
+	if (nota1 >= nota2) {
+		System.out.println("Nota 1 é maior e igual");
+	}else {
+		System.out.println("as notas sao iguais");
+	}
+	
 	}
 
 }
