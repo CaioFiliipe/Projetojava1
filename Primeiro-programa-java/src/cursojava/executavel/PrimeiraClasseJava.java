a5ef3660307b574e4d8414711be34937f5556c3d
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-24 22:22:40 -0300

    Operador ternário simples

--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,14 +5,14 @@ public class PrimeiraClasseJava {
 	/* main é um metodo auto ecxecutavel em java */
 	public static void main(String[] args) {
 
-		int nota1 = 10;
-		int nota2 = 60;
-		int nota3 = 10;
-		int nota4 = 10;
+		int nota1 = 50;
+		int nota2 = 90;
+		int nota3 = 40;
+		int nota4 = 20;
 		int media = 0;
 
 		media = (nota1 + nota2 + nota3 + nota4) / 4;
-
+//Condiçoes logicas com IF e ELSE
 		if (media >= 70) {
 			System.out.println("aluno Aprovado: " + media);
 		}
@@ -21,6 +21,10 @@ public class PrimeiraClasseJava {
 		} else {
 			System.out.println("Aluno Reprovado: " + media);
 		}
-
+       
+		//operadores ternarios sao para micro validaçoes
+		
+		String saidaResultado = media >= 70 ? "aluno aprovado" : "aluno reprovado";
+		System.out.println(saidaResultado);
 	}
 }
