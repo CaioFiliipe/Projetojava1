5351cad4dcc3fd0069f8b638dca61217ebbba663
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-25 21:29:15 -0300

    Estrutura de repetição e parada com Break

--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,10 +4,14 @@ public class PrimeiraClasseJava {
 
 	/* main é um metodo auto ecxecutavel em java */
 	public static void main(String[] args) {
-  //Estrutura de repetiçao FOR
+  //Estrutura de repetiçao FOR com breack "parada"
 	
-		for(int numero = 100; numero >=99; numero --) {
-		System.out.println("numero atual é :" +numero);	
+		for(int numero = 0; numero <=10; numero ++) {
+		if (numero == 3) {
+			System.out.println("Obaaa, encontrei o numero 3");
+			System.out.println("estou parando de executar");
+			break;
+		}
 		}
 	}
 
