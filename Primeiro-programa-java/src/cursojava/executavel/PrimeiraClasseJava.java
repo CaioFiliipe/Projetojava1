9fbee9dd3f12cac93d669e0ba98da9ac1afb56ca
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-25 22:43:23 -0300

    Criando nossa entrada de dados

--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,16 +1,23 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiraClasseJava {
 
 	/* main é um metodo auto ecxecutavel em java */
 	public static void main(String[] args) {
    // Resto da divisão
-		 
-		 double moto = 9;
-		 double pessoa = 2;
-		 
-		 double resto = moto % pessoa;
-		 System.out.println("soubaram exatamente :" + resto + " carro ");
+	String carros =	 JOptionPane.showInputDialog("Informe a quantidade de carros");
+	String pessoas =	 JOptionPane.showInputDialog("Informe a quantidade de pessoas");
+	double carroNumero = Double.parseDouble(carros);
+	double pessoaNumero = Double.parseDouble(pessoas);
+	
+	int divisao = (int) (carroNumero / pessoaNumero);//quando ele fez o calculo ele descobrou que ficava 4,5 ,mas como eu connvertir para inteiro entao ele so mostra o inteiro 
+	
+	double resto = carroNumero % pessoaNumero; // aqui ele mostra o resto da divisao
+	//System.out.println("Divisao para Pessoas " + divisao + " carros e sobrou " + resto + " carros");
+	JOptionPane.showMessageDialog(null, "Divisao para Pessoas deu " + divisao + " carros e sobrou " + resto + " carros");
+	System.out.println(carros);
 	}
 
 }
