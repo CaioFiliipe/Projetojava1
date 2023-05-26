234d3db91cfadad4e646aed3f40352ca8b456795
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-25 22:11:28 -0300

    Divisões e resto com MOD do Java

--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,15 +4,13 @@ public class PrimeiraClasseJava {
 
 	/* main é um metodo auto ecxecutavel em java */
 	public static void main(String[] args) {
-  //Estrutura de repetiçao FOR e continue
-	
-		for(int numero = 0; numero <=10; numero ++) {
-		if (numero == 3 || numero == 6 || numero == 9  ) {
-			System.out.println("Obaaa, encontrei o numero " +numero);
-			continue;
-	}
-	System.out.println("processando laçoo de repetiçao");
-	}
+   // Resto da divisão
+		 
+		 double moto = 9;
+		 double pessoa = 2;
+		 
+		 double resto = moto % pessoa;
+		 System.out.println("soubaram exatamente :" + resto + " carro ");
 	}
 
 }
