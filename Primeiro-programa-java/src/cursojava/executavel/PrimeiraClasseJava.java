13995e63e47ec0f3a25475759f11cf90c9b0b26e
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-25 21:05:59 -0300

    Estrutura de repetição FOR

--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,22 +4,11 @@ public class PrimeiraClasseJava {
 
 	/* main é um metodo auto ecxecutavel em java */
 	public static void main(String[] args) {
-  //Estrutura de repetiçao while
+  //Estrutura de repetiçao FOR
 	
-		int numero = 0;
-		
-		while (numero <= 10) { //verifica e depois  executa o codigo
-			System.out.println("O numero atual é : " + numero);
-			numero++;
+		for(int numero = 100; numero >=99; numero --) {
+		System.out.println("numero atual é :" +numero);	
 		}
-		/*----------------------------------------*/
-		int numero2 = 0;
-	
-		do { //primeiro ele executa depois verifica
-			System.out.println("O numero atual é : " + numero2);
-			numero2++;
-		}while(numero2 <= 60);
-		
 	}
 
 }
