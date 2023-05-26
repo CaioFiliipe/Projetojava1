06fabe875036ccd352ce24544abda462d19e2b2c
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-25 23:06:27 -0300

    Criando uma opção de confirmação

--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -9,15 +9,28 @@ public class PrimeiraClasseJava {
    // Resto da divisão
 	String carros =	 JOptionPane.showInputDialog("Informe a quantidade de carros");
 	String pessoas =	 JOptionPane.showInputDialog("Informe a quantidade de pessoas");
+	
 	double carroNumero = Double.parseDouble(carros);
 	double pessoaNumero = Double.parseDouble(pessoas);
 	
 	int divisao = (int) (carroNumero / pessoaNumero);//quando ele fez o calculo ele descobrou que ficava 4,5 ,mas como eu connvertir para inteiro entao ele so mostra o inteiro 
 	
 	double resto = carroNumero % pessoaNumero; // aqui ele mostra o resto da divisao
-	//System.out.println("Divisao para Pessoas " + divisao + " carros e sobrou " + resto + " carros");
-	JOptionPane.showMessageDialog(null, "Divisao para Pessoas deu " + divisao + " carros e sobrou " + resto + " carros");
-	System.out.println(carros);
+	
+	int resposta =JOptionPane.showConfirmDialog(null, "Deseja ver o Resultado da divisão?");
+	
+	if(resposta == 0) {
+		JOptionPane.showMessageDialog(null, "Divisao para Pessoas deu " + divisao);
+	}else {
+		System.out.println("Não quiz ver o resultado");
+	}
+	//System.out.println("Divisao para Pessoas " + divisao + " carros e sobrou " + resto + " carros");		
+	 resposta =JOptionPane.showConfirmDialog(null, "Deseja ver o Resto da divisão?");
+	if (resposta == 0) {
+		JOptionPane.showConfirmDialog(null, "O Resto da divosão é " + resto);		
+	}else {
+		System.out.println("Não quiz ver o resultado");
+	}
 	}
 
 }
