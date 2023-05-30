a4fc26da39f3f6f7b330e69e90f96ea2f5f06160
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-29 21:19:25 -0300

    Primeira Classe e Obejto aluno

---
+++ Primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,17 @@
+package cursojava.classes;
+
+//Estra é nosa classe/obejto que representa o aluno
+public class Aluno {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registoGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
+}
