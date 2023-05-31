bc58360bce5945102e38ace45b783a6fef3e5c7d
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-31 20:51:19 -0300

    método de calculo de média de nota usando Set e Get

--- Primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ Primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -11,6 +11,13 @@ public class Aluno {
 	private String dataMatricula;
 	private String serieMatriculado;
 	private String nomeEscola;
+    
+	private double nota1;
+    private double nota2;
+    private double nota3;
+    private double nota4;
+
+
 
    public Aluno() { //cria os dados na memoria 
 	
@@ -110,8 +117,43 @@ public String getNomeEscola() {
 public void setNomeEscola(String nomeEscola) {
 	this.nomeEscola = nomeEscola;
 }
+
+public double getNota1() {
+	return nota1;
+}
+
+public void setNota1(double nota1) {
+	this.nota1 = nota1;
+}
+
+public double getNota2() {
+	return nota2;
+}
+
+public void setNota2(double nota2) {
+	this.nota2 = nota2;
+}
+
+public double getNota3() {
+	return nota3;
+}
+
+public void setNota3(double nota3) {
+	this.nota3 = nota3;
+}
+
+public double getNota4() {
+	return nota4;
+}
+
+public void setNota4(double nota4) {
+	this.nota4 = nota4;
+}
   
-  
+//metodo que retorna a media do aluno
+  public double getMediaNota() {
+	  return (nota1 + nota2 + nota3 + nota4) / 4;
+  }
   
 }
    
--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -20,7 +20,11 @@ public class PrimeiraClasseJava {
     aluno1.setDataMatricula("10/01/2019");
     aluno1.setSerieMatriculado("5a");
     aluno1.setNomeEscola("Escola Java");   
-        
+    aluno1.setNota1(90);
+    aluno1.setNota2(80.8);
+    aluno1.setNota3(70.9);
+    aluno1.setNota4(90.7);
+    
     System.out.println("Nome é = " + aluno1.getNome());
     System.out.println("Idade é = " + aluno1.getIdade());
     System.out.println("Data de Nascimento é = " + aluno1.getDataNascimento());
@@ -31,23 +35,128 @@ public class PrimeiraClasseJava {
     System.out.println("A Data de matricula foi em = " + aluno1.getDataMatricula());
     System.out.println("Serie Matriculada  = " + aluno1.getSerieMatriculado());
     System.out.println("Nome da Escola Matriculada  é = " + aluno1.getNomeEscola());
-
+    System.out.println("Media da nota é = " +aluno1.getMediaNota());
     System.out.println("-----------------------------------------------------------");
-    //.........................................
     
-	Aluno aluno2 = new Aluno();//aqui sera o pedro
-	aluno2.setNome("Pedro");
-	aluno2.setIdade(40);
-	aluno2.setDataNascimento("15/19/1995");
+    
+	Aluno aluno2 = new Aluno();
+	aluno2.setNome("Pedro henrrique");
+	aluno2.setIdade(29);
+	aluno2.setDataNascimento("22/09/2006");
+	aluno2.setRegistroGeral("787.3268.87");
+	aluno2.setNumeroCpf("000.324.000.00");
+	aluno2.setNomeMae("Maria");
+	aluno2.setNomePai("Cleber");
+	aluno2.setDataMatricula("21/02/2019");
+	aluno2.setSerieMatriculado("6a");
+	aluno2.setNomeEscola("Escola Programação");   
+	aluno2.setNota1(97);
+	aluno2.setNota2(88.8);
+	aluno2.setNota3(72.9);
+	aluno2.setNota4(87.7);
 	
 	System.out.println("Nome é = " + aluno2.getNome());
     System.out.println("Idade é = " + aluno2.getIdade());
     System.out.println("Data de Nascimento é = " + aluno2.getDataNascimento());
+    System.out.println("Numero do Registo geral é = " + aluno2.getRegistroGeral());
+    System.out.println("Numero do CPF é = " + aluno2.getNumeroCpf());
+    System.out.println("Nome da Mãe é = " + aluno2.getNomeMae());
+    System.out.println("Nome do Pai é = " + aluno2.getNomePai());
+    System.out.println("A Data de matricula foi em = " + aluno2.getDataMatricula());
+    System.out.println("Serie Matriculada  = " + aluno2.getSerieMatriculado());
+    System.out.println("Nome da Escola Matriculada  é = " + aluno2.getNomeEscola());
+    System.out.println("Media da nota é = " + aluno2.getMediaNota());
+	System.out.println("----------------------------------------------------------------");
 	
-    //---------------------------
-	Aluno aluno3 = new Aluno();//aqui sera o Caio
-    Aluno aluno4 = new Aluno("Maria"); 
-	Aluno aluno5 = new Aluno("José", 50);
+	   
+	Aluno aluno3 = new Aluno();
+	aluno3.setNome("Mateus Sampaio");
+	aluno3.setIdade(32);
+	aluno3.setDataNascimento("22/09/2006");
+	aluno3.setRegistroGeral("787.3268.87");
+	aluno3.setNumeroCpf("000.324.000.00");
+	aluno3.setNomeMae("Joana");
+	aluno3.setNomePai("Silvio");
+	aluno3.setDataMatricula("21/02/2019");
+	aluno3.setSerieMatriculado("3a");
+	aluno3.setNomeEscola("Escola Padrao");   
+	aluno3.setNota1(72.6);
+	aluno3.setNota2(82.8);
+	aluno3.setNota3(73.5);
+	aluno3.setNota4(81.7);
+	
+	System.out.println("Nome é = " + aluno3.getNome());
+    System.out.println("Idade é = " + aluno3.getIdade());
+    System.out.println("Data de Nascimento é = " + aluno3.getDataNascimento());
+    System.out.println("Numero do Registo geral é = " + aluno3.getRegistroGeral());
+    System.out.println("Numero do CPF é = " + aluno3.getNumeroCpf());
+    System.out.println("Nome da Mãe é = " + aluno3.getNomeMae());
+    System.out.println("Nome do Pai é = " + aluno3.getNomePai());
+    System.out.println("A Data de matricula foi em = " + aluno3.getDataMatricula());
+    System.out.println("Serie Matriculada  = " + aluno3.getSerieMatriculado());
+    System.out.println("Nome da Escola Matriculada  é = " + aluno3.getNomeEscola());
+    System.out.println("Media da nota é = " + aluno3.getMediaNota());
+	System.out.println("-------------------------------------------------------------");
+	
+	
+    Aluno aluno4 = new Aluno(); 
+    aluno4.setNome("Caio filipe");
+    aluno4.setIdade(27);
+    aluno4.setDataNascimento("14/09/1995");
+    aluno4.setRegistroGeral("787.3268.87");
+    aluno4.setNumeroCpf("000.324.000.00");
+    aluno4.setNomeMae("Simone luiz");
+    aluno4.setNomePai("José Robson");
+    aluno4.setDataMatricula("21/02/2019");
+    aluno4.setSerieMatriculado("9a");
+    aluno4.setNomeEscola("Escola De linguagem Java");   
+    aluno4.setNota1(99.6);
+    aluno4.setNota2(99.8);
+    aluno4.setNota3(99.5);
+    aluno4.setNota4(99.7);
+	
+	System.out.println("Nome é = " + aluno4.getNome());
+    System.out.println("Idade é = " + aluno4.getIdade());
+    System.out.println("Data de Nascimento é = " + aluno4.getDataNascimento());
+    System.out.println("Numero do Registo geral é = " + aluno4.getRegistroGeral());
+    System.out.println("Numero do CPF é = " + aluno4.getNumeroCpf());
+    System.out.println("Nome da Mãe é = " + aluno4.getNomeMae());
+    System.out.println("Nome do Pai é = " + aluno4.getNomePai());
+    System.out.println("A Data de matricula foi em = " + aluno4.getDataMatricula());
+    System.out.println("Serie Matriculada  = " + aluno4.getSerieMatriculado());
+    System.out.println("Nome da Escola Matriculada  é = " + aluno4.getNomeEscola());
+    System.out.println("Media da nota é = " + aluno4.getMediaNota());
+    System.out.println("-----------------------------------------------------------------");
+           
+	Aluno aluno5 = new Aluno();
+	aluno5.setNome("Junior Santos");
+	aluno5.setIdade(27);
+	aluno5.setDataNascimento("14/01/1993");
+	aluno5.setRegistroGeral("787.3268.00");
+	aluno5.setNumeroCpf("111.324.000.00");
+	aluno5.setNomeMae("Suelane luiz");
+	aluno5.setNomePai("Lopes Robson");
+	aluno5.setDataMatricula("21/02/2019");
+	aluno5.setSerieMatriculado("9a");
+	aluno5.setNomeEscola("Escola De linguagem ");   
+	aluno5.setNota1(92.4);
+	aluno5.setNota2(77.3);
+	aluno5.setNota3(80.2);
+	aluno5.setNota4(79.1);
+		
+		System.out.println("Nome é = " + aluno5.getNome());
+	    System.out.println("Idade é = " + aluno5.getIdade());
+	    System.out.println("Data de Nascimento é = " + aluno5.getDataNascimento());
+	    System.out.println("Numero do Registo geral é = " + aluno5.getRegistroGeral());
+	    System.out.println("Numero do CPF é = " + aluno5.getNumeroCpf());
+	    System.out.println("Nome da Mãe é = " + aluno5.getNomeMae());
+	    System.out.println("Nome do Pai é = " + aluno5.getNomePai());
+	    System.out.println("A Data de matricula foi em = " + aluno5.getDataMatricula());
+	    System.out.println("Serie Matriculada  = " + aluno5.getSerieMatriculado());
+	    System.out.println("Nome da Escola Matriculada  é = " + aluno5.getNomeEscola());
+	    System.out.println("Media da nota é = " + aluno5.getMediaNota());
+	    System.out.println("-----------------------------------------------------------------");
+	           
 	
 	}
 
