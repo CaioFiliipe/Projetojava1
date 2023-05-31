4b27f54e78d1a06daa3bb8ddb4ecbbf886042542
Geyse <Geyse@DESKTOP-R42U865.Dlink>
2023-05-30 23:42:13 -0300

    Os metódos Getters e Setters

--- Primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ Primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -1,33 +1,120 @@
 package cursojava.classes;
-
-//Estra é nosa classe/obejto que representa o aluno
+//esta é nossa classe/obejto que representa o aluno
 public class Aluno {
-	//caio
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String RegistroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String serieMatriculado;
+	private String nomeEscola;
+
+   public Aluno() { //cria os dados na memoria 
 	
-	//Esses são os atributos do Aluno.
-	public  String nome;
-	 public int idade;
-	 public String dataNascimento;
-	String registoGeral;
-	String numeroCpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMatriculado;
-	
-	public Aluno() {//cria dados na memoria - sendo padrão do java
-		
-		
-	}
-	
-	public Aluno (String nomePadrao) {
-		nome = nomePadrao;
-	}
-	public Aluno (String nomePadrao , int idadePadrao) {
-	 nome = nomePadrao;
-	 idade = idadePadrao;
-	 {
-		 }		 
-	 }
+
+   }
+   
+  public Aluno (String nomePadrao) {
+	  nome = nomePadrao;
+  }
+	 
+  public Aluno (String nomePadrao,int idadePadrao) {
+	  nome =nomePadrao;
+	  idade = idadePadrao;
+  }
+  
+  //metodos SETTERS E GETTERS DO Obejto
+  //SET é para adicionar ou receber dados para os atributos
+  // GET é para resgar ou obeter o valor do atributo
+  
+  //Recebe dados
+  public void setNome(String nome) {
+	  this.nome = nome;
+  }
+  
+  public String getNome() {
+	  return nome;
+  }
+
+public int getIdade() {
+	return idade;
 }
+
+public void setIdade(int idade) {
+	this.idade = idade;
+}
+
+public String getDataNascimento() {
+	return dataNascimento;
+}
+
+public void setDataNascimento(String dataNascimento) {
+	this.dataNascimento = dataNascimento;
+}
+
+public String getRegistroGeral() {
+	return RegistroGeral;
+}
+
+public void setRegistroGeral(String registroGeral) {
+	RegistroGeral = registroGeral;
+}
+
+public String getNumeroCpf() {
+	return numeroCpf;
+}
+
+public void setNumeroCpf(String numeroCpf) {
+	this.numeroCpf = numeroCpf;
+}
+
+public String getNomeMae() {
+	return nomeMae;
+}
+
+public void setNomeMae(String nomeMae) {
+	this.nomeMae = nomeMae;
+}
+
+public String getNomePai() {
+	return nomePai;
+}
+
+public void setNomePai(String nomePai) {
+	this.nomePai = nomePai;
+}
+
+public String getSerieMatriculado() {
+	return serieMatriculado;
+}
+
+public void setSerieMatriculado(String serieMatriculado) {
+	this.serieMatriculado = serieMatriculado;
+}
+
+public String getDataMatricula() {
+	return dataMatricula;
+}
+
+public void setDataMatricula(String dataMatricula) {
+	this.dataMatricula = dataMatricula;
+}
+
+public String getNomeEscola() {
+	return nomeEscola;
+}
+
+public void setNomeEscola(String nomeEscola) {
+	this.nomeEscola = nomeEscola;
+}
+  
+  
+  
+}
+   
+    
+
+
--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -3,28 +3,52 @@ package cursojava.executavel;
 import cursojava.classes.Aluno;
 
 public class PrimeiraClasseJava {
-//caio
+
 	// main é um metodo auto ecxecutavel em java
 	public static void main(String[] args) {
-   
-	//New Aluno() é uma instancia (Criação de Obejtos)
-	//aluno1 é uma referencia para o Obejto Aluno
-		Aluno aluno1 = new Aluno(); // aqui será o joão
-	 	 aluno1.nome = "João";
-	 	 aluno1.idade = 50;
-	 	
-	 	 System.out.println("Idade do aluno 1 é = " + aluno1.idade);	 	 
-	 	 System.out.println("Nome do aluno 1 é = "  + aluno1.nome);
-		
-		
-	 	 
-	 	 Aluno aluno2 = new Aluno(); // aqui será o pedro
+   		
+	
+	Aluno aluno1 = new Aluno();//aqui sera o joao	
+	
+    aluno1.setNome("João da silva");
+    aluno1.setIdade(50);
+    aluno1.setDataNascimento("14/09/1995");
+    aluno1.setRegistroGeral("787.8787.87");
+    aluno1.setNumeroCpf("000.000.000.00");
+    aluno1.setNomeMae("Simone");
+    aluno1.setNomePai("robson");
+    aluno1.setDataMatricula("10/01/2019");
+    aluno1.setSerieMatriculado("5a");
+    aluno1.setNomeEscola("Escola Java");   
+        
+    System.out.println("Nome é = " + aluno1.getNome());
+    System.out.println("Idade é = " + aluno1.getIdade());
+    System.out.println("Data de Nascimento é = " + aluno1.getDataNascimento());
+    System.out.println("Numero do Registo geral é = " + aluno1.getRegistroGeral());
+    System.out.println("Numero do CPF é = " + aluno1.getNumeroCpf());
+    System.out.println("Nome da Mãe é = " + aluno1.getNomeMae());
+    System.out.println("Nome do Pai é = " + aluno1.getNomePai());
+    System.out.println("A Data de matricula foi em = " + aluno1.getDataMatricula());
+    System.out.println("Serie Matriculada  = " + aluno1.getSerieMatriculado());
+    System.out.println("Nome da Escola Matriculada  é = " + aluno1.getNomeEscola());
 
-		Aluno aluno3 = new Aluno(); // aqui será o caio
-	    
-		Aluno Aluno4 = new Aluno ("Maria");
-		
-		Aluno Aluno5 = new Aluno("José" ,50);
+    System.out.println("-----------------------------------------------------------");
+    //.........................................
+    
+	Aluno aluno2 = new Aluno();//aqui sera o pedro
+	aluno2.setNome("Pedro");
+	aluno2.setIdade(40);
+	aluno2.setDataNascimento("15/19/1995");
+	
+	System.out.println("Nome é = " + aluno2.getNome());
+    System.out.println("Idade é = " + aluno2.getIdade());
+    System.out.println("Data de Nascimento é = " + aluno2.getDataNascimento());
+	
+    //---------------------------
+	Aluno aluno3 = new Aluno();//aqui sera o Caio
+    Aluno aluno4 = new Aluno("Maria"); 
+	Aluno aluno5 = new Aluno("José", 50);
+	
 	}
 
 }
